345865101e3189e5e2664a446ed36e0e56d19871
Brian Hudi <bshudi@gmail.com>
2018-11-13 14:51:00 -0600

    updated posts location

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,11 +38,11 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const indexPage = path.resolve('./src/templates/index.jsx');
-    const postPage = path.resolve('./src/templates/post.jsx');
-    const tagPage = path.resolve('./src/templates/tag.jsx');
-    const categoryPage = path.resolve('./src/templates/category.jsx');
-    const authorPage = path.resolve('./src/templates/author.jsx');
+    const indexPage = path.resolve('src/templates/index.jsx');
+    const postPage = path.resolve('src/templates/post.jsx');
+    const tagPage = path.resolve('src/templates/tag.jsx');
+    const categoryPage = path.resolve('src/templates/category.jsx');
+    const authorPage = path.resolve('src/templates/author.jsx');
 
     if (
       !fs.existsSync(
--- src/components/Navigation/GetNavList.jsx
+++ src/components/Navigation/GetNavList.jsx
@@ -34,7 +34,7 @@ function GetNavList(config) {
   });
 
   NavList.push({
-    primaryText: 'Contact Me',
+    primaryText: 'Contact',
     component: Link,
     to: '/contact/'
   });
