d18b6f4198795d1c9ecb3efa399403469693562b
Brian <bshudi@gmail.com>
2019-05-12 22:51:25 -0500

    added new static form for mailchimp

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -24,11 +24,67 @@
       <slot />
     </main>
 
-    <footer class="footer">
-      <socials></socials>
-      <span class="footer__copyright">
-        Copyright Brian Hudi {{ new Date().getFullYear() }}.
-      </span>
+    <footer class="footer mt2em">
+      <div class="footer__content">
+        <!-- Begin Mailchimp Signup Form -->
+        <link
+          href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <div id="mc_embed_signup" class="content-box">
+          <form
+            action="https://gmail.us20.list-manage.com/subscribe/post?u=559f90f3ea4519beae72cbc60&amp;id=54c95bd002"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            target="_blank"
+            novalidate
+          >
+            <div id="mc_embed_signup_scroll">
+              <label for="mce-EMAIL">Subscribe for weekly updates!</label>
+              <input
+                type="email"
+                value=""
+                name="EMAIL"
+                class="email full-width"
+                id="mce-EMAIL"
+                placeholder="email address"
+                required
+              />
+              <div
+                style="position: absolute; left: -5000px;"
+                aria-hidden="true"
+              >
+                <input
+                  type="text"
+                  name="b_559f90f3ea4519beae72cbc60_54c95bd002"
+                  tabindex="-1"
+                  value=""
+                />
+              </div>
+              <div class="clear">
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button primary-button full-width"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <!--End mc_embed_signup-->
+        <div class="flex-column white-text socials">
+          <socials></socials>
+          <span class="footer__copyright">
+            Copyright Brian Hudi {{ new Date().getFullYear() }}.
+          </span>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -55,6 +111,21 @@ export default {
 </script>
 
 <style lang="scss">
+#mc-embedded-subscribe-form {
+  padding: 0 !important;
+}
+
+#mc_embed_signup {
+  @media screen and (max-width: 800px) {
+    width: 100%;
+  }
+  margin: 0;
+}
+
+.full-width {
+  width: 100% !important;
+}
+
 .z10 {
   z-index: 10;
 }
@@ -102,13 +173,29 @@ export default {
 
 .footer {
   grid-area: footer;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  background: var(--header-gradient);
   padding: calc(var(--space) / 2);
   text-align: center;
   font-size: 0.8em;
+
+  &__content {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: auto;
+    max-width: 800px;
+    padding: 0 15px;
+
+    @media screen and (max-width: 800px) {
+      flex-direction: column;
+      width: 100%;
+      min-width: 100%;
+      .socials {
+        align-items: center;
+      }
+    }
+  }
 
   > span {
     margin: 0 0.35em;
@@ -117,5 +204,9 @@ export default {
   a {
     color: currentColor;
   }
+}
+
+.socials {
+  align-items: flex-end;
 }
 </style>
